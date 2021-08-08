79c395ee69aae08cdc4ced5777267280b88b55a5
Md Belal Uddin <65133988+Belal-uddin@users.noreply.github.com>
2021-08-08 22:12:27 +0600

    Update dfs_code.cpp
    A raw dfs code .

--- Graph_algorithm/dfs_code.cpp
+++ Graph_algorithm/dfs_code.cpp
@@ -1,1 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+vector<int>graph[100000];
+bool vis[100000];
 
+void dfs(int x){
+    vis[x]=true;
+    cout<<x<<" ";// instantly  printing nodes when visited. 
+    for(int i=0;i<graph[x].size();i++){
+        int now = graph[x][i];
+        if(!vis[now])
+            {
+                vis[now]=true;
+                dfs(now);
+            }
+    }
+}
+int main()
+{   
+
+   int n,m;
+   cin>>n>>m;
+   // n is number of nodes and m is number of columns
+   int root=-1;
+    while(m--){
+        ll int  x,y;cin>>x>>y;
+
+        graph[x].push_back(y);
+        graph[y].push_back(x);
+
+         if(root<0)root=x; // taking the first node as root node.it may differ code to code.
+    }
+    dfs(root);
+   
+
+}
