a41aa91de265f5b75e1cea5a2715c4fc11f629b3
Md Belal Uddin <65133988+Belal-uddin@users.noreply.github.com>
2022-01-04 22:44:07 +0600

    Delete inserting_node_at_beginning.c

--- Data-Structure/Linked_List/inserting_node_at_beginning.c
+++
@@ -1,9 +0,0 @@
-void insert_at_beginning(int data)
-{
-	struct node *newNode;
-	newNode = (struct node *)malloc(sizeof(struct node));
-
-	newNode->data = data;
-	newNode->next = head;
-	head = newNode;
-}
