24ff96e43274f433ca1a80b980d77d20c1630586
Md Belal Uddin <65133988+Belal-uddin@users.noreply.github.com>
2022-01-04 22:41:18 +0600

    Create creating_and_traversing.c

---
+++ Data-Structure/Linked_List/singly_linked_list/creating_and_traversing.c
@@ -0,0 +1,71 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+struct node{
+	int data;
+	struct node *next;
+}*head;
+
+void createlist(int n);
+void displaylist();
+
+int main()
+{
+
+	 int data,n;
+	 printf("Enter number of nodes:");
+	 scanf("%d",&n);
+	 
+	 createlist(n);
+
+	 printf("The data of the list : ");
+	 displaylist();
+
+}
+
+void createlist(int n)
+{
+	struct node *newNode,*temp;
+	int data;
+
+	head = (struct node *)malloc(sizeof(struct node));
+	printf("enter data for node 1:");
+	scanf("%d",&data);
+
+	head->data = data;
+	head->next = NULL;
+    temp = head;
+	for(int i=2;i<=n;i++)
+	{
+		newNode = (struct node *)malloc(sizeof(struct node));
+		printf("Enter data for node %d: ",i);
+		scanf("%d",&data);
+
+		newNode->data = data;
+		newNode->next = NULL;
+		temp->next = newNode;
+		temp = temp->next;
+	}
+
+}
+
+
+void displaylist()
+{
+	struct node *temp;
+
+	if(head==NULL)
+	{
+		printf("List is empty.\n");
+	}
+	else{
+		temp = head;
+
+		while(temp!=NULL)
+		{
+			printf("%d ",temp->data);
+			temp = temp->next;
+		}
+	}
+}
